4f54ebf94cbef3d8e1bffa274acad538ff657c44
ahmed-gimy <ahmedadam5757@gmail.com>
2023-07-13 19:21:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with puts function
+ *
+ * Return: Alawys 0 (Succcess)
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
