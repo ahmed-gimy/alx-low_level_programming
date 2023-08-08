7d0d722e1a655c3133ca3ee90107417538375ddb
ahmed-gimy <ahmedadam5757@gmail.com>
2023-08-08 10:43:44 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *create_array - creat an array of chars
+ * @size: array size
+ * @c: char
+ * return: pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *n = malloc(size);
+
+	if (size == 0 || n == 0)
+		return (0);
+
+	while (size--)
+		n[size] = c;
+
+	return (n);
+}
